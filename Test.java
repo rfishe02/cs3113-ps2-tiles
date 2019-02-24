6843f87527daf4802bdc5ae7d6bd68b070d36ae2
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-02-24 01:41:53 -0600

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,41 +1,45 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
-		Problem g = new Problem();
-		
-		// Create the inital problem state.
-		
-		Problem p = new Problem();
-		p.setInitialState(3);
-		p.setGoalState(3);
 
-		// Create a starting node with the inital state & a path cost of zero.
+		// Run the test & print the average result.
+		double correct = 0;
+		int total = 0;
 		
-		Node s = new Node(0);
-		s.setDeepCopy(p.start);
-		s.pos = p.getStartPoint();
-		
-		// Run the test & print the result.
-		
-		Node res = aStar(p,s);
-		
-		if(res != null) {
-			printBoard(res.board);
+		for(int i = 0; i < 10; i++) {
 			
-			Node current = res;
-			while(current.parent != null) {
-				System.out.println(current.action[0]+" "+current.action[1]);
-				current = current.parent;
+			// Create the inital problem state.
+			
+			Problem p = new Problem();
+			p.setInitialState(3);
+			p.setGoalState(3);
+
+			// Create a starting node with the inital state & a path cost of zero.
+			
+			Node s = new Node(0);
+			s.setDeepCopy(p.start);
+			s.pos = p.getStartPoint();
+			
+			Node res = aStar(p,s);
+			
+			if(res != null) {
+				solution(p,res);
+				correct++;
+			} else {
+				System.out.println("No solution found.");
 			}
 			
-		} else {
-			System.out.println("No solution found.");
+			total++;
 		}
+		
+		System.out.println(correct/total+"");
 		
 	}
 
@@ -118,6 +122,37 @@ public class Test {
 		child.pathCost = parent.pathCost + p.stepCost(child); // Determine the cost of the action.
 		
 		return child;
+	}
+	
+	public static void solution(Problem p, Node n) {
+		Stack<String> s = new Stack<>();
+
+		Node current = n;
+		while(current.parent != null) {
+			s.push(getLabel(current.action));
+			current = current.parent;
+		}
+		
+		printBoard(p.start);
+
+		while(!s.isEmpty()) {
+			System.out.print(s.pop()+" ");
+		}
+		System.out.println();
+	}
+	
+	public static String getLabel(int[] action) {
+		
+		if(action[0] == -1) {
+			return "UP";
+		} else if(action[0] == 1) {
+			return "DOWN";
+		} else if(action[1] == -1) {
+			return "LEFT";
+		} else {
+			return "RIGHT";
+		}
+		
 	}
 		
 	//////
